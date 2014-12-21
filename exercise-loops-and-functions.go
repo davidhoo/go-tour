4762d85364b5aaab2d514823f20c25fe8215353a
davidhoo <coolbor@gmail.com>
2014-12-22 00:41:24 +0800

    completed func

--- exercise-loops-and-functions.go
+++ exercise-loops-and-functions.go
@@ -2,11 +2,19 @@ package main
 
 import (
     "fmt"
+    "math"
 )
 
 func Sqrt(x float64) float64 {
+    z := 1.0
+    for i := 0; i < 10; i++ {
+        z = z - (z*z-x)/(z*2)
+        fmt.Println(">>>>>>>", z)
+    }
+    return z
 }
 
 func main() {
-    fmt.Println(Sqrt(2))
+    fmt.Println("my func:", Sqrt(2))
+    fmt.Println("math.Sqrt:", math.Sqrt(2))
 }

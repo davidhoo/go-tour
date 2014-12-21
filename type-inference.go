881a87e20f436768f83e1b189fff66ca5a0dd324
davidhoo <coolbor@gmail.com>
2014-12-21 23:11:17 +0800

    add files

---
+++ type-inference.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+    v := 42 // change me!
+    f := 3.142
+    g := 0.867 + 0.05i
+    s := "I'am strings ^_^"
+    fmt.Printf("v is of type %T\n", v)
+    fmt.Printf("f is of type %T\n", f)
+    fmt.Printf("g is of type %T\n", g)
+    fmt.Printf("s is of type %T\n", s)
+}

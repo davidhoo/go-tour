4a31fda294d013fd55435669e081e2eb3a9942e3
davidhoo <coolbor@gmail.com>
2014-12-23 01:07:55 +0800

    add functions

---
+++ function-fibonacci-closure.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func fibonacci() func() int {
+    x, y := 1, 1
+    return func() int {
+        x, y = y, x+y
+        return x
+    }
+}
+
+func main() {
+    f := fibonacci()
+
+    for i := 0; i < 10; i++ {
+        fmt.Println(f())
+    }
+}

2b12d36f868a932da4518247cfcdfc9741b09e7b
davidhoo <coolbor@gmail.com>
2014-12-23 00:25:24 +0800

    add slices

---
+++ exercise-slices.go
@@ -0,0 +1,20 @@
+package main
+
+import "code.google.com/p/go-tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+    matrix := make([][]uint8, dy)
+
+    for i := 0; i < dy; i++ {
+        matrix[i] = make([]uint8, dx)
+        for j := 0; j < dx; j++ {
+            matrix[i][j] = uint8((i^j) * (i^j))
+        }
+    }
+
+    return matrix
+}
+
+func main() {
+    pic.Show(Pic)
+}

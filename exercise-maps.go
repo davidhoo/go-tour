cf3e8779ec3ec680c8e3b0355ad22d32c17fd147
davidhoo <coolbor@gmail.com>
2014-12-23 00:43:14 +0800

    add maps

---
+++ exercise-maps.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "code.google.com/p/go-tour/wc"
+    "strings"
+)
+
+func WordCount(s string) map[string]int {
+    words := strings.Fields(s)
+    m := make(map[string]int)
+
+    for _,word := range words {
+        m[word]++
+    }
+    return m
+}
+
+func main() {
+    wc.Test(WordCount)
+}

e86875c628d30adecbb162b99757a144abcc31b5
davidhoo <coolbor@gmail.com>
2014-12-22 22:19:47 +0800

    重构循环，改固定10次为达到条件自动跳出

--- exercise-loops-and-functions.go
+++ exercise-loops-and-functions.go
@@ -7,14 +7,17 @@ import (
 
 func Sqrt(x float64) float64 {
     z := 1.0
-    for i := 0; i < 10; i++ {
-        z = z - (z*z-x)/(z*2)
-        fmt.Println(">>>>>>>", z)
+    for {
+        if math.Abs(z - (z - (z*z-x)/(z*2))) < 0.00000000000001 {
+            return z
+        } else {
+            z = z - (z*z-x)/(z*2)
+            fmt.Println(">>>>>z is:", z)
+        }
     }
-    return z
 }
 
 func main() {
-    fmt.Println("my func:", Sqrt(2))
+    fmt.Println("  my func:", Sqrt(2))
     fmt.Println("math.Sqrt:", math.Sqrt(2))
 }

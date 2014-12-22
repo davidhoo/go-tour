83059ccfbeb15e40d760e19f7b03b08d20be276a
davidhoo <coolbor@gmail.com>
2014-12-22 22:32:44 +0800

    add switch

---
+++ switch.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+    "runtime"
+)
+
+func main() {
+    fmt.Println("Go runs on ")
+    switch os := runtime.GOOS; os {
+    case "darwin":
+        fmt.Println("OS x.")
+    case "linux":
+        fmt.Println("Linux.")
+    default:
+        fmt.Println("%s.", os)
+    }
+}

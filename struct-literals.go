f836a4da637a6fdca4c760702d7e3c4ece55de26
davidhoo <coolbor@gmail.com>
2014-12-22 22:58:23 +0800

    add struct

---
+++ struct-literals.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+    X, Y int
+}
+
+var (
+    v1 = Vertex{1, 2}
+    v2 = Vertex{X: 1}
+    v3 = Vertex{}
+    p = &Vertex{10, 2}
+)
+
+func main() {
+    fmt.Println(v1, v2, v3, p, p.X)
+}
---
+++ struct-pointers.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+    X int
+    Y int
+}
+
+func main() {
+    v := Vertex{1, 2}
+    p := &v
+    p.X = 99
+    fmt.Println(v)
+}

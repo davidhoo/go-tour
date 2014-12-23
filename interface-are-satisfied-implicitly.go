0077d10a3d9b66ce336959627e1d5e223043dfc0
davidhoo <coolbor@gmail.com>
2014-12-23 10:45:25 +0800

    add interface

---
+++ interface-are-satisfied-implicitly.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "fmt"
+    "os"
+)
+
+type Reader interface {
+    Read(b []byte) (n int, err error)
+}
+
+type Writer interface {
+    Write(b []byte) (n int, err error)
+}
+
+type ReadWriter interface {
+    Reader
+    Writer
+}
+
+func main() {
+    var w Writer
+
+    w = os.Stdout
+
+    fmt.Fprintf(w, "hello, world!\n")
+}
---
+++ interfaces.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+type Abser interface {
+    Abs() float64
+}
+
+type MyFloat float64
+
+func (f MyFloat) Abs() float64 {
+    if f < 0 {
+        return float64(-f)
+    }
+    return float64(f)
+}
+
+type Vertex struct {
+    X, Y float64
+}
+
+func (v *Vertex) Abs() float64 {
+    return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func main() {
+    var a Abser
+
+    f := MyFloat(-math.Sqrt2)
+    v := Vertex{3, 4}
+
+    a = f
+    b := &v
+
+    fmt.Println(a.Abs(), b.Abs())
+}

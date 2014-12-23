c0e43e2fce957f4bbcc99134a095cc72acab8dcf
davidhoo <coolbor@gmail.com>
2014-12-24 00:12:55 +0800

    add stringer interface

---
+++ exercise-stringer.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type IPAddr [4]byte
+
+func (ipAddr IPAddr) String() string {
+    return fmt.Sprintf("%v.%v.%v.%v", ipAddr[0], ipAddr[1], ipAddr[2], ipAddr[3])
+}
+
+
+func main() {
+    addrs := map[string]IPAddr{
+        "lookback": {127, 0, 0, 1},
+        "googleDNS": {8, 8, 8, 8},
+    }
+
+    for n, a := range addrs {
+        fmt.Printf("%v: %v\n", n ,a)
+    }
+}

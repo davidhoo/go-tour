8428bc961cdc45d35d264a71c4195b00a7e47987
davidhoo <coolbor@gmail.com>
2014-12-28 23:22:47 +0800

    images

---
+++ exercise-images.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "code.google.com/p/go-tour/pic"
+    "image"
+    "image/color"
+)
+
+type Image struct{
+    w, h int
+    c uint8
+}
+
+func (img *Image) Bounds() image.Rectangle {
+    return image.Rect(0, 0, img.w, img.h)
+}
+
+func (img *Image) ColorModel() color.Model {
+    return color.RGBAModel
+}
+
+func (img *Image) At(x , y int) color.Color {
+    return color.RGBA{img.c + uint8(x*y+x*y), img.c + uint8(x*y+x*y), 255, 255}
+}
+
+func main() {
+    m := Image{100, 100, 128}
+    pic.ShowImage(&m)
+}

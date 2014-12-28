28fa958e33e927ff47a18e90806c774b9d6adc50
davidhoo <coolbor@gmail.com>
2014-12-28 21:44:12 +0800

    add reader

---
+++ exercise-reader.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "code.google.com/p/go-tour/reader"
+)
+
+type MyReader struct{}
+
+func (r MyReader) Read(b []byte) (int, error) {
+    for i := 0; i < len(b); i++ {
+        b[i] = 'A'
+    }
+    return len(b), nil
+}
+
+func main() {
+    reader.Validate(MyReader{})
+}
---
+++ exercise-rot-reader.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "io"
+    "os"
+    "strings"
+)
+
+type rot13Reader struct {
+    r io.Reader
+}
+
+func (rot *rot13Reader) Read(b []byte) (int, error) {
+    n, e := rot.r.Read(b)
+
+    for i := 0; i < n; i++ {
+        if (b[i] >= 'A' && b[i] < 'N') || (b[i] >= 'a' && b[i] < 'n') {
+            b[i] += 13
+        } else if (b[i] > 'M' && b[i] <= 'Z') || (b[i] > 'm' && b[i] <= 'z') {
+            b[i] -= 13
+        }
+    }
+    return n, e
+}
+
+func main() {
+    s := strings.NewReader("Lbh penpxrq gur pbqr!")
+    r := rot13Reader{s}
+    io.Copy(os.Stdout, &r)
+}

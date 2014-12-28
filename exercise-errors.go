724a4b64147200d65c63d549f8de9689b19a19b2
davidhoo <coolbor@gmail.com>
2014-12-28 12:27:50 +0800

    add errors

---
+++ exercise-errors.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+    "math"
+    "fmt"
+)
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+    return fmt.Sprintf("can not Sqrt negative number:%v", float64(e))
+}
+
+/*
+func Sqrt(x float64) (float64, error) {
+    return 0, nil
+}
+*/
+func Sqrt(x float64) (float64, error) {
+    if x < 0 {
+        return 0, ErrNegativeSqrt(x)
+    }
+    z := 1.0
+    for {
+        if math.Abs(z - (z - (z*z-x)/(z*2))) < 0.00000000000001 {
+            return z, nil
+        } else {
+            z = z - (z*z-x)/(z*2)
+        }
+    }
+}
+
+func main() {
+    fmt.Println(Sqrt(2))
+    fmt.Println(Sqrt(-2))
+}

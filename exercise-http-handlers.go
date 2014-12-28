b23e6a924f10edf13d606e64c1c461b7b8acf537
davidhoo <coolbor@gmail.com>
2014-12-28 22:30:37 +0800

    add http

---
+++ exercise-http-handlers.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "log"
+    "net/http"
+    "fmt"
+)
+
+type String string
+
+type Struct struct{
+    Greeting string
+    Punct    string
+    Who      string
+}
+
+func (s String) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, s)
+}
+
+func (s Struct) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, s)
+}
+
+func main() {
+    http.Handle("/string", String("I'm a frayed knot."))
+    http.Handle("/struct", &Struct{"Hello", ":", "Gophers!"})
+    log.Fatal(http.ListenAndServe("localhost:4000", nil))
+}
